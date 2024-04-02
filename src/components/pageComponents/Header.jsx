dce1eeaf55d7253a4aca1e12977779e2f0f23b22
Brandon Taylor <156858984+brandontaylor1@users.noreply.github.com>
2024-04-02 00:12:32 -0500

    Add Styling to Header in Homepage

--- src/components/pageComponents/Header.jsx
+++ src/components/pageComponents/Header.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import './Header.css'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
-        <nav>
-            <img src='NFL LOGO' alt='nfl logo' />
-            <h1>Player Tracker</h1>
-        </nav>
-      
+      <nav>
+        <div className='navbrand'>
+          <img sr='#' alt='logo' />
+          <h1>Player Tracker</h1>
+        </div>
+        <div>
+          <NavLink to='/homepage' activeClassName='active'>Home</NavLink>
+          <NavLink to='/teams' activeClassName='active'>Teams</NavLink>
+          <NavLink to='/players' activeClassName='active'>Players</NavLink>
+        </div>
+      </nav>    
     </header>
   )
 }
